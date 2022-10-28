954335bae854f4fd354f785303b25347da717f56
zheng <lisa-marie.zheng@dauphine.eu>
2022-10-28 17:20:39 +0200

    modif Catalog

--- src/main/java/com/miage/vehicle_sales_management/model/shop/Catalog.java
+++ src/main/java/com/miage/vehicle_sales_management/model/shop/Catalog.java
@@ -1,5 +1,16 @@
 package com.miage.vehicle_sales_management.model.shop;
 
+import com.miage.vehicle_sales_management.model.cars.Vehicle;
+
+import java.util.ArrayList;
+
 public class Catalog {
+
+    private ArrayList<Vehicle> catalog;
+
+    public Catalog(){
+        
+    }
+    // Constructor
 
 }
